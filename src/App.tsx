10433f59d43e9e6c573d31f2ca74b9e0ec74098b
ebina <ebina.engr@gmail.com>
2024-09-29 18:57:39 +0900

    canvasを拡大、カメラの位置を調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
-import './App.css';
 import Box from "./components/Box";
 
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas
+        camera={{ fov: 70, near: 0.1, far: 2000 }}
+        style={{ width: "100vw", height: "100vh" }}
+      >
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <Box position={[-2, 0, 0]} />
