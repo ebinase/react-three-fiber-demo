ddd200053de529cf16eb7a38289bf8c1ed252866
ebina <ebina.engr@gmail.com>
2024-10-01 02:18:02 +0900

    宇宙船と衛星を実装

--- src/components/space/Space.tsx
+++ src/components/space/Space.tsx
@@ -1,16 +1,40 @@
-import { OrbitControls, Stars, useTexture } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import {OrbitControls, Stars, useGLTF, useTexture } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 import { FC, useRef } from "react";
-import { Group } from "three";
+import { Group, Mesh, Vector3 } from "three";
 
 const Space: FC = () => {
   const earthRef = useRef({} as Group);
   const starsRef = useRef({} as Group);
-  useFrame(() => {
+  const satelliteRef = useRef({} as Mesh);
+  const starshipRef = useRef({} as Mesh);
+  const { camera, size } = useThree();
+  useFrame((state) => {
     earthRef.current.rotation.y += 0.003;
     starsRef.current.rotation.y += 0.0003;
+
+    const t = state.clock.getElapsedTime();
+    satelliteRef.current.position.x = 1.2 * Math.cos(t);
+    satelliteRef.current.position.z = 1.2 * Math.sin(t);
+    satelliteRef.current.lookAt(0,0,0);
+
+    // スクリーン座標での位置（例えば、右下に固定したい場合）
+    const screenX = size.width * 0.5;
+    const screenY = size.height * (0.8 + 0.02 * Math.sin(t));
+
+    // スクリーン座標をワールド座標に変換
+    const vector = new Vector3(
+      (screenX / size.width) * 2 - 1,
+      -(screenY / size.height) * 2 + 1,
+      0
+    ).unproject(camera);
+
+    // モデルの位置を更新
+    starshipRef.current.position.set(vector.x, vector.y, vector.z);
+    starshipRef.current.lookAt(0,0,0);
   });
   const texture = useTexture("/src/assets/earch1024x512.png");
+  const { scene } = useGLTF('/src/assets/starship.gltf')
   return (
     <>
       <color args={["#000"]} attach={"background"} />
@@ -35,9 +59,21 @@ const Space: FC = () => {
       <group ref={earthRef}>
         <mesh>
           <sphereGeometry args={[1, 32, 32]} />
-          <meshPhongMaterial map={texture} emissive={"white"} emissiveIntensity={0.01} />
+          <meshPhongMaterial
+            map={texture}
+            emissive={"white"}
+            emissiveIntensity={0.01}
+          />
         </mesh>
       </group>
+      {/* 軌道上を回るキューブ */}
+      <mesh ref={satelliteRef} position={[1.2, 0, 0]}>
+        <boxGeometry  args={[.1, .1, .1]} />
+        <meshPhongMaterial />
+      </mesh>
+      {/* 宇宙船 */}
+      {/* download from https://market.pmnd.rs/model/low-poly-spaceship */}
+      <primitive scale={.02} ref={starshipRef} object={scene}/>
     </>
   );
 };
