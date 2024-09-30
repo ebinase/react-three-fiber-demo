efc46206a8301f0b9409618b9f5fb929acac63fb
ebina <ebina.engr@gmail.com>
2024-09-30 19:14:39 +0900

    地球のテクスチャを貼り自転させた

--- src/components/space/Space.tsx
+++ src/components/space/Space.tsx
@@ -1,21 +1,42 @@
-import { OrbitControls, Sphere, Stars } from "@react-three/drei";
+import { OrbitControls, Stars, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { FC, useRef } from "react";
 import { Group } from "three";
 
 const Space: FC = () => {
-  const ref = useRef({} as Group);
+  const earthRef = useRef({} as Group);
+  const starsRef = useRef({} as Group);
   useFrame(() => {
-    ref.current.rotation.y += 0.001;
+    earthRef.current.rotation.y += 0.003;
+    starsRef.current.rotation.y += 0.0003;
   });
+  const texture = useTexture("/src/assets/earch1024x512.png");
   return (
     <>
       <color args={["#000"]} attach={"background"} />
-      <directionalLight position={[0, 0, 500]} color={"0xffffff"} />
       <OrbitControls />
-      <group ref={ref}>
+      {/* 太陽光 */}
+      <directionalLight
+        position={[100, 50, 100]}
+        intensity={7}
+        color={"white"}
+      />
+      {/* 裏側も見えるようにするためのライト */}
+      <directionalLight
+        position={[-100, -100, -100]}
+        intensity={0.5}
+        color={"white"}
+      />
+      {/* 背景の星たち */}
+      <group ref={starsRef}>
         <Stars />
-        <Sphere></Sphere>
+      </group>
+      {/* 自転する地球 */}
+      <group ref={earthRef}>
+        <mesh>
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshPhongMaterial map={texture} />
+        </mesh>
       </group>
     </>
   );
